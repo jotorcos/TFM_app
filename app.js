033b03697691556beaf46107637a4afaaebd96bd
Josep Tormo <josep.tormo@pivotree.com>
2023-05-30 11:43:55 +0200

    Add api key

--- app.js
+++ app.js
@@ -110,6 +110,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function getMeteorologicalData(latitude, longitude) {
+    const apiKey = 'c3034e02ba05fd3aec1bc080872ba8c6';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
     return fetch(apiUrl)
